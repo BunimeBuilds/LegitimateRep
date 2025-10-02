dcac70ce64d7695af0c33e70899fe8071c765713
BunimeBuilds <bunime@outlook.com>
2025-10-02 12:39:19 -0700

    feat: Add getReputationData External View Function

--- src/onchain/TestLegitRep.sol
+++ src/onchain/TestLegitRep.sol
@@ -139,4 +139,11 @@ contract TestLegitRep {
         }
         return _reputations[user].score;
     }
+
+    function getReputationData(address user) external view returns (ReputationData memory) {
+        if (!_reputations[user].isRegistered) {
+            return ReputationData({score: INITIAL_REPUTATION, totalRatings: 0, isRegistered: false});
+        }
+        return _reputations[user];
+    }
 }
