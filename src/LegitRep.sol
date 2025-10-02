41702209d3e4b8a35baf0cf40c606e3d1fa0eeb5
BunimeBuilds <bunime@outlook.com>
2025-10-02 09:21:18 -0700

    feat: Add ReputationUpdated Event

--- src/LegitRep.sol
+++ src/LegitRep.sol
@@ -46,5 +46,10 @@ contract LegitRep is Ownable, ReentrancyGuard {
 
     // Events
     event UserRegistered(address indexed user, uint256 initialReputation);
-    
+        event ReputationUpdated(
+        address indexed user, 
+        uint256 oldScore, 
+        uint256 newScore, 
+        address indexed rater
+    );
 }
